37e7c3391904b407b8e69ccc3bca246b36cc7d98
Charlie Blake <charlieb@graphcore.ai>
2023-04-11 16:48:09 +0000

    Add MLP and supporting fns

--- unit_scaling/functional.py
+++ unit_scaling/functional.py
@@ -2,15 +2,60 @@
 
 """Unit-scaled versions of common `torch.nn.functional` functions."""
 
-from typing import Callable, Optional
+from typing import Any, Callable, Optional
 
 import numpy as np
 import torch.nn.functional as F
 from torch import Tensor
 
 from .constraints import gmean
-from .docs import docstring_from, unary_constraint_docstring
+from .docs import docstring_from, format_docstring, unary_constraint_docstring
 from .scale import scale_bwd, scale_fwd
+
+
+@format_docstring(unary_constraint_docstring)
+def scale_elementwise(
+    f: Callable[..., Tensor],
+    output_scale: float,
+    grad_input_scale: float,
+    constraint: Optional[Callable[[float, float], float]] = gmean,
+) -> Callable[..., Tensor]:
+    """Transforms an element-wise function into a scaled version.
+
+    Args:
+        f (Callable[..., Tensor]): the element-wise function to be scaled. Should take
+            as its first input a `Tensor`, followed by `*args, **kwargs`.
+        output_scale (float): the scale to be applied to the output
+        grad_input_scale (float): the scale to be applied to the grad of the input
+        {0}
+
+    Returns:
+        Callable[..., Tensor]: the scaled function
+    """
+    if constraint:
+        output_scale = grad_input_scale = constraint(output_scale, grad_input_scale)
+
+    def scaled_f(input: Tensor, *args: Any, **kwargs: Any) -> Tensor:
+        input = scale_bwd(input, grad_input_scale)
+        output = f(input, *args, **kwargs)
+        return scale_fwd(output, output_scale)
+
+    return scaled_f
+
+
+@docstring_from(
+    F.gelu,
+    short_description="Applies a **unit-scaled** GELU function.",
+    add_args=[unary_constraint_docstring],
+)
+def gelu(
+    input: Tensor,
+    constraint: Optional[Callable[[float, float], float]] = gmean,
+) -> Tensor:
+    output_scale = 0.588**-1
+    grad_input_scale = 0.675**-1
+    scaled_gelu = scale_elementwise(F.gelu, output_scale, grad_input_scale, constraint)
+    return scaled_gelu(input)
 
 
 @docstring_from(
--- unit_scaling/tests/test_modules.py
+++ unit_scaling/tests/test_modules.py
@@ -1,15 +1,27 @@
 # Copyright (c) 2023 Graphcore Ltd. All rights reserved.
 
+import pytest
 import torch
 from torch.optim import SGD
 
-from ..modules import Linear
+from ..modules import GELU, MLP, Linear
 from .helper import (
     assert_not_unit_scaled,
     assert_unit_scaled,
     unit_backward,
     unit_normal,
 )
+
+
+def test_gelu() -> None:
+    input = unit_normal(2**10)
+    model = GELU()
+    output = model(input)
+
+    unit_backward(output)
+
+    combined_std = output.std().detach() * input.grad.std()  # type: ignore
+    assert combined_std == pytest.approx(1, abs=0.1)
 
 
 def test_linear() -> None:
@@ -24,5 +36,29 @@ def test_linear() -> None:
     unit_backward(output)
     SGD(model.parameters(), lr=1).step()
 
+    combined_std = output.std().detach() * input.grad.std()  # type: ignore
+    assert combined_std == pytest.approx(1, abs=0.1)
+
     assert_not_unit_scaled(model.weight)
     assert torch.any(model.bias != 0)
+
+
+def test_mlp() -> None:
+    input = unit_normal(2**8, 2**10)
+    model = MLP(2**10)
+    output = model(input)
+
+    assert_unit_scaled(model.linear_1.weight, model.linear_2.weight)
+    assert torch.all(model.linear_1.bias == 0)
+    assert torch.all(model.linear_2.bias == 0)
+    assert output.shape == torch.Size([2**8, 2**10])
+
+    unit_backward(output)
+    SGD(model.parameters(), lr=1).step()
+
+    combined_std = output.std().detach() * input.grad.std()  # type: ignore
+    assert combined_std == pytest.approx(1, abs=0.1)
+
+    assert_not_unit_scaled(model.linear_1.weight, model.linear_2.weight)
+    assert torch.any(model.linear_1.bias != 0)
+    assert torch.any(model.linear_2.bias != 0)
