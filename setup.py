f1a0e6f7b4d0796b45d470f0ddb1339707bcbbc9
Douglas Orr <douglaso@graphcore.ai>
2023-07-10 06:33:16 +0000

    Add IPU support to unit_scaling.formats

--- setup.py
+++ setup.py
@@ -4,9 +4,20 @@ from pathlib import Path
 
 import setuptools
 
+requirements = Path("requirements.txt").read_text().rstrip("\n").split("\n")
+try:
+    import poptorch
+
+    # This should match requirements-dev-ipu.txt
+    requirements.append(
+        "git+https://github.com/graphcore-research/poptorch-experimental-addons@beb12678d1e7ea2c033bd061d32167be262dfa58"
+    )
+except ImportError:
+    pass
+
 setuptools.setup(
     name="unit-scaling",
     description="A library for unit scaling in PyTorch.",
     packages=["unit_scaling"],
-    install_requires=Path("requirements.txt").read_text().rstrip("\n").split("\n"),
+    install_requires=requirements,
 )
--- unit_scaling/tests/test_formats.py
+++ unit_scaling/tests/test_formats.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2023 Graphcore Ltd. All rights reserved.
 
+import collections
+
 import torch
 
 from ..formats import FPFormat
@@ -8,6 +10,7 @@ from ..formats import FPFormat
 def test_fp_format() -> None:
     fmt = FPFormat(2, 1)
     assert fmt.bits == 4
+    assert str(fmt) == "E2M1-SR"
     assert fmt.max_absolute_value == 3
     assert fmt.min_absolute_normal == 0.5
     assert fmt.min_absolute_subnormal == 0.25
@@ -17,6 +20,22 @@ def test_fp_format() -> None:
     assert set(
         FPFormat(3, 0).quantise_fwd(torch.linspace(-10, 10, steps=1000)).abs().tolist()
     ) == {0, 0.125, 0.25, 0.5, 1, 2, 4, 8}
+
+
+def test_fp_format_rounding() -> None:
+    n = 10000
+    x = -1.35
+
+    y_nearest = FPFormat(2, 1, rounding="nearest").quantise(torch.full((n,), x))
+    assert collections.Counter(y_nearest.tolist()) == {-1.5: n}
+
+    y_stochastic = FPFormat(2, 1, rounding="stochastic").quantise(torch.full((n,), x))
+    count = collections.Counter(y_stochastic.tolist())
+    assert count.keys() == {-1.5, -1.0}
+    expected_ratio = (1.35 - 1.0) / 0.5
+    nearest_ratio = count[-1.5] / sum(count.values())
+    std_x3 = 3 * (expected_ratio * (1 - expected_ratio) / n) ** 0.5
+    assert expected_ratio - std_x3 < nearest_ratio < expected_ratio + std_x3
 
 
 def test_fp_format_bwd() -> None:
